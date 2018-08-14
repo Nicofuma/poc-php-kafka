b2ba571f7ce8cdf23b571117b0da10a7e5c5b3a1
Stefaan Lippens <stefaan@netlog.com>
2018-08-14 18:22:59 +0200

    simplify python consumer loop

--- python/src/consume.py
+++ python/src/consume.py
@@ -1,26 +1,27 @@
 import sys
 import traceback
+import contextlib
 from confluent_kafka import KafkaError
 from confluent_kafka.avro import AvroConsumer
 from confluent_kafka.avro.serializer import SerializerError
 
-c = AvroConsumer({'bootstrap.servers': 'kafka:9092', 'group.id': 'group1', 'schema.registry.url': 'http://schemaregistry:8081'})
+c = AvroConsumer({
+    'bootstrap.servers': 'kafka:9092',
+    'group.id': 'group1',
+    'schema.registry.url': 'http://schemaregistry:8081',
+})
 topic = 'page_visits'
 c.subscribe([topic])
 
 print("Starting to consume kafka topic {t}".format(t=topic))
-running = True
-while running:
-    try:
+
+with contextlib.closing(c):
+    while True:
         msg = c.poll(10)
         if msg:
             if not msg.error():
                 print(msg.value())
             elif msg.error().code() != KafkaError._PARTITION_EOF:
                 print(msg.error())
-                running = False
-    except SerializerError as e:
-        print("Message deserialization failed for %s: %s" % (msg, e))
-        running = False
+                break
 
-c.close()
